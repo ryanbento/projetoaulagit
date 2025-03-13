7528335da14f2b4e8ef4c32860f2188ba5d50f1e
Ryan Bento <bentopreciliano@gmail.com>
2025-03-13 11:43:56 -0300

    meu primeiro commite

---
+++ my-site/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ return ( 
+  <>
+    <h1 className='titulo'>Titulo</h1>
+    
+  </>
+ )
+}
+
+export default App
---
+++ my-site/src/aul.py
@@ -0,0 +1,29 @@
+
+# valor_da_compra = float(input('Digite o valor da compra'))
+# if 500>valor_da_compra>1000
+#     descconto = valor_da_compra * 0.05
+#     valor_final = valor_da_compra-descconto
+#     print(valor_da_compra)
+
+Nome = input('Bem vindo ao mundo da programação, João')
+print(Nome)
+
+
+
+Valor_na_carteira = int(input('Digite o valor na carteira: '))
+cambio = Valor_na_carteira/5
+print(f"O valorem dolar é de: {cambio}")
+
+
+Numero01 = int(input('Digite o primeiro número: '))
+Numero02 = int(input('Digite o segundo número: '))
+Numero03 = int(input('Digite o terceiro número: '))
+soma = Numero01*Numero02*Numero03
+print(f'o resultado é: {soma}')
+
+
+
+
+
+
+
